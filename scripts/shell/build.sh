649a959de66934ebcd73bfb19803525c9ea25a9c
LeeSeika <leeseika@outlook.com>
2024-08-30 18:16:48 +0800

    optimize build.sh

--- scripts/shell/build.sh
+++ scripts/shell/build.sh
@@ -21,16 +21,28 @@ fi
 # gen paginator
 protoc ./proto/paginator/paginator.proto --proto_path=./proto/ --go_out=paths=source_relative:./pb
 
-# gen service code
-services=(user post)
+# gen biz service code
+biz_services=(user post)
 
-for item in "${services[@]}"; do
+for item in "${biz_services[@]}"; do
   goctl api go -api ./api/"$item".api -dir ./service/"$item"/api/
   goctl rpc protoc ./proto/"$item"/"$item".proto --proto_path=./proto/ --go_out=./pb --go-grpc_out=./pb --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative --zrpc_out=./service/"$item"/rpc/
 done
 
-# build service
-for item in "${services[@]}"; do
+# build biz service
+for item in "${biz_services[@]}"; do
   GOOS=${GOOS} GOARCH=${GOARCH} go build  -o ../target/"$item"-rpc ./service/"$item"/rpc/"$item".go
   GOOS=${GOOS} GOARCH=${GOARCH} go build  -o ../target/"$item"-api ./service/"$item"/api/"$item".go
+done
+
+# gen base service code
+base_services=(leaf)
+
+for item in "${base_services[@]}"; do
+  goctl rpc protoc ./proto/"$item"/"$item".proto --proto_path=./proto/ --go_out=./pb --go-grpc_out=./pb --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative --zrpc_out=./service/"$item"/rpc/
+done
+
+#build base service
+for item in "${base_services[@]}"; do
+  GOOS=${GOOS} GOARCH=${GOARCH} go build  -o ../target/"$item"-rpc ./service/"$item"/rpc/"$item".go
 done
