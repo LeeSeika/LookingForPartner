f7fc0e0d7761b7cc3b4511844974f84133ed129f
LeeSeika <leeseika@outlook.com>
2024-09-25 15:20:10 +0800

    update log sync path

--- src/common/logger/logger.go
+++ src/common/logger/logger.go
@@ -9,7 +9,7 @@ func NewLogger(ctx context.Context, service string) logx.Logger {
 	conf := logx.LogConf{
 		ServiceName: service,
 		Mode:        "file",
-		Path:        "logs/" + service,
+		Path:        "home/logs/" + service,
 		KeepDays:    7,
 		MaxBackups:  2,
 		Rotation:    "daily",
